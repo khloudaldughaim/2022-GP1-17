aa35e659c77cfd6749a7303014873de7fcc63f9f
saraAlkhrayef <105759506+saraAlkhrayef@users.noreply.github.com>
2022-10-19 00:40:05 +0300

    ignore this

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
